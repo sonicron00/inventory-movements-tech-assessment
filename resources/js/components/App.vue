51553ee2ba0f55df879df1b99a3b0a8b7e5298c8
Ryan Smith <=>
2023-07-21 14:02:56 +0100

    Upgrade to PHP8.1 and Laravel 10

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -2,11 +2,11 @@
   <main>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <router-link to="/" class="navbar-brand" href="#">Figured Technical Assessment - Inventory Management | Ryan
-          Smith 2022
+        <router-link to="/" class="navbar-brand" href="#">Figured x Ryan - 2023 GLOW-UP
         </router-link>
         <div class="collapse navbar-collapse">
           <div class="navbar-nav">
+            <router-link exact-active-class="active" to="/bonus" class="nav-item nav-link">NEW 2023 Update</router-link>
             <router-link exact-active-class="active" to="/" class="nav-item nav-link">Product Application</router-link>
             <router-link exact-active-class="active" to="/products" class="nav-item nav-link">Product Admin</router-link>
             <router-link exact-active-class="active" to="/transactions" class="nav-item nav-link">Transactions</router-link>
